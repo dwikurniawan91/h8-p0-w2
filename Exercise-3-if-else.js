b9269aca10b829fff25695b540ddd1c37c68608d
dwikurniawan91 <dui.dict@gmail.com>
2019-08-27 23:38:24 +0700

    Add file Exercise-2

---
+++ Exercise-3-if-else.js
@@ -0,0 +1,17 @@
+var nama = 'Zero';
+var peran = 'Penyihir';
+
+if (nama === '') {
+    console.log('Nama harus  diisi!');
+} else if (nama === 'Nina' && peran === ''){
+    console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
+} else if (nama === 'Nina' && peran === 'Ksatria') {
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}
+    Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`);
+} else if (nama === 'Danu' && peran === 'Tabib') {
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}
+    Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`);
+} else if (nama === 'Zero' && peran === 'Penyihir') {
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}
+    Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+}
