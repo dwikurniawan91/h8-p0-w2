885e0fde6b16c210632b4c96be67b5bef9dc3816
dwikurniawan91 <dui.dict@gmail.com>
2019-08-28 12:56:01 +0700

    rubah dikit

--- Exercise-7-Looping-bintang.js
+++ Exercise-7-Looping-bintang.js
@@ -3,30 +3,30 @@
 
 
 console.log('No 1');
-function soal1 (banyak) {
-    for (var k = 1; k <= banyak ; k++) {
+function soal1 (height1) {
+    for (var k = 1; k <= height1 ; k++) {
         console.log('*');
     }
 }
 soal1(5);
 
 console.log('No 2');
-function soal2 (height) {
-    for (var m = 1; m <= height; m++) {
+function soal2 (height2) {
+    for (var m = 1; m <= height2; m++) {
       var str = '';
-    for (var n = 1; n <= height; n++) {
+    for (var n = 1; n <= height2; n++) {
         str += '*';
     }
 console.log(str);
     }
 }
-soal2(5);
+soal2(6);
 
 
 
 console.log('No 3');
-function soal3 (height) {
-    for (var i = 1; i <= height; i++) {
+function soal3 (height3) {
+    for (var i = 1; i <= height3; i++) {
     var row = '';// 
     for (var j = 1; j <= i; j++) {
         row += '*';
@@ -34,4 +34,4 @@ function soal3 (height) {
     console.log(row);
     }
 }
-soal3(5);
+soal3(10);
