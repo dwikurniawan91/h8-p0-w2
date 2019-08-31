423390d4330f80db59ffba5030c42fd6b5cbabd9
dwikurniawan91 <dui.dict@gmail.com>
2019-08-31 15:28:19 +0700

    modify Exercise-4-Switch-case

--- Exercise-4-Switch-case.js
+++ Exercise-4-Switch-case.js
@@ -1,20 +1,58 @@
-var hari = 1;
-var bulan = 1;
-var tahun = 2018;
+var hari = 31;
+var bulan = 12;
+var tahun = 3000;
 
-switch (true) {
-    case ((hari === 12) && (bulan === 12) && tahun === 2019):
-        console.log(`${hari} Desember ${tahun}`);
+if(hari > 0 && hari <=31 && tahun >0 && tahun <= 10000){
+    hari === hari;
+    tahun === tahun;
+
+    switch (true) {
+    case ((hari === hari) && (bulan === 1) && tahun === tahun):
+        console.log(`${hari} Januari ${tahun}`);
         break;
-    case ((hari === 1) && (bulan === 1) && tahun === 2018):
-            console.log(`${hari} Januari ${tahun}`);
-            break;
-    case ((hari === 20) && (bulan === 12) && tahun === 2011):
-        console.log(`${hari} Desember ${tahun}`);
+    case ((hari === hari) && (bulan === 2) && tahun === tahun):
+        console.log(`${hari} Februari ${tahun}`);
         break;
-    case ((hari === 3) && (bulan === 8) && tahun === 2018):
-            console.log(`${hari} Agustus ${tahun}`);
-            break;
+    case ((hari === hari) && (bulan === 3) && tahun === tahun):
+        console.log(`${hari} Maret ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 4) && tahun === tahun):
+        console.log(`${hari} April ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 5) && tahun === tahun):
+        console.log(`${hari} Mei ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 6) && tahun === tahun):
+        console.log(`${hari} Juni ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 7) && tahun === tahun):
+        console.log(`${hari} Juli ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 8) && tahun === tahun):
+        console.log(`${hari} Agustus ${tahun}`)
+        break;
+    case ((hari === hari) && (bulan === 9) && tahun === tahun):
+        console.log(`${hari} September ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 10) && tahun === tahun):
+        console.log(`${hari} Oktober ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 11) && tahun === tahun):
+        console.log(`${hari} November ${tahun}`);
+        break;
+    case ((hari === hari) && (bulan === 12) && tahun === tahun):
+        console.log(`${hari} Desember ${tahun}`)
+        break;
     default:
-        console.log('Masukkan tanggal, bulan dan tahun');     
+        console.log('Input invalid');
+    }
+}else{
+    console.log('Input invalid');
 }
+
+
+
+
+
+
+
